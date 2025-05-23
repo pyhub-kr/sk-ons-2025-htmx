4656620ef2c445812202aeeceb873fef18e1e691
Chinseok Lee <me@pyhub.kr>
2025-05-23 17:32:50 +0900

    settings.DATABASES 설정에 DATABASE_URL 환경변수 반영

--- config/settings.py
+++ config/settings.py
@@ -114,10 +114,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(),
 }
 
 
